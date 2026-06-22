7e6a410f121eacb3933509564d900287c5c39226
diegoarteagamendoza <daarteagamendoza@gmail.com>
2026-06-22 14:38:53 -0400

    configuración para build

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://127.0.0.1:8000/api'
-    // apiUrl: 'https://proyectofonoaudiologiafaceubb2026.onrender.com/api' // Aquí pones la URL real cuando hagas el deploy
+    // apiUrl: 'http://127.0.0.1:8000/api'
+    apiUrl: 'https://proyectofonoaudiologiafaceubb2026.onrender.com/api' // Aquí pones la URL real cuando hagas el deploy
 };
